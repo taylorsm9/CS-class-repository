6873731a5cda9b29bb257c3eaa1ad242c429824d
taylorsm9 <59939625+taylorsm9@users.noreply.github.com>
2020-01-15 17:28:42 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/*
+*Java Lab 1
+*Scott Taylor
+*1/15/20
+*CMSC 255 901
+*/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Your Name>");
+		System.out.println("<Java Project Name>");
+		System.out.println("<Project Description>");
+		System.out.println("<Version Date>");
+		System.out.println("<Course Number and Section>");
+		System.out.println();
+		}
+	}
